7b033eac864f22b081a052ed31560cfb10138391
yoniakabecky <yony.aoki@gmail.com>
2019-02-24 22:08:53 -0800

    Did some adjustment

--- java/src/Week1/ForLoops/FizzBuzz.java
+++ java/src/Week1/ForLoops/FizzBuzz.java
@@ -7,11 +7,15 @@ public class FizzBuzz {
             boolean buzz = false;
             String result = "";
 
-            if (i % 3 == 0) fizz = true;
-            if (i % 5 == 0) buzz = true;
+            if (i % 3 == 0) {
+                fizz = true;
+                result = result + "Fizz";
+            }
+            if (i % 5 == 0) {
+                buzz = true;
+                result = result + "Buzz";
+            }
 
-            if (fizz) result = result + "Fizz";
-            if (buzz) result = result + "Buzz";
             if (!fizz && !buzz) result = Integer.toString(i);
 
             System.out.println(result);
