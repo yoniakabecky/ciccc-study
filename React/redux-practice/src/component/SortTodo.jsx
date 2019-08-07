bfc139d61d948689f6e11842bba50d28d73a5316
yoniakabecky <yony.aoki@gmail.com>
2019-08-07 12:22:41 -0700

    Catchup

---
+++ React/redux-practice/src/component/SortTodo.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+class SortTodo extends React.Component {
+  render() {
+    return (
+      <div>
+        <button>All</button>
+        <button>Done</button>
+        <button>Not Yet</button>
+      </div>
+    )
+  }
+}
+
+export default SortTodo;
