5113d2d9b0a64b37bcd79d8639d24de181358091
yoniakabecky <yoniakabecky@gmail.com>
2019-09-11 12:39:54 -0700

    update

--- Node/express_3_recap/index.js
+++ Node/express_3_recap/index.js
@@ -1,62 +1,34 @@
 const express = require('express');
 const recipes = require('./models/recipes');
 const uuid = require('uuid/v1');
-
+const mongoose = require('mongoose');
+require('dotenv').config();
 const app = express();
 
-// 3) Body-parser middleware
+
+// Body-parser middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-// 2) getting list -> check localhost:5000/api/recipes 
-app.get('/api/recipes', (req, res) => {
-    // res.send("Recipes List")
-    // 4) make data file recipes.js import recipes
-    res.json(recipes);
-});
 
-// 5) post: add new data, install uuid for unique id
-app.post('/api/recipes', (req, res) => {
-    const { image_url, name } = req.body;
+// Connect to mongoDB
+const ATLAS_URL = process.env.ATLAS_URL;
 
-    const newRecipes = {
-        id: uuid(),
-        image_url: image_url,
-        name: name,
-    };
+mongoose.connect(ATLAS_URL, { 
+    useNewUrlParser: true, 
+    useCreateIndex: true,
+    useUnifiedTopology: true
+})
 
-    console.log(image_url, name, recipes.push(newRecipes));
-    res.json(recipes);
-});
+const connection = mongoose.connection;
+connection.once('open', () => {
+    // when connection succeeds
+    console.log('mongoDB Connection worked!');
+})
 
-// 6) put: update the data, 
-app.put('/api/recipes/:id', (req, res) => {
-    const { image_url, name } = req.body;
-    
-    // find the data of the id, 
-    const updatingId = req.params.id;
 
-    // update that object
-    const index = recipes.findIndex(recipe => recipe.id === updatingId);
-
-    recipes[index] = {
-        id: updatingId,
-        image_url: image_url,
-        name: name,
-    }
-
-    // response = updated list
-    res.json(recipes)
-
-});
-
-// // 7) delete,
-app.delete('/api/recipes/:id', (req, res) => {
-    const deleteId = req.params.id;
-    const filterd = recipes.filter(recipe => recipe.id !== deleteId);
-
-    res.json(filterd);
-});
+// Routes, make file and cut n paste
+app.use('/api/recipes', require('./routes/recipes'));
 
 
 const PORT = process.env.PORT || 5000;
